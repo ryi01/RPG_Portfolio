a334c3d74af99ac6b15e55da31445688bea45a8b
ryi01 <kkjmo3@naver.com>
2026-02-02 22:09:05 +0900

    [Feat]input Attack추가

---
+++ Assets/KMK/Script/Player/InputAttack.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class InputAttack : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
