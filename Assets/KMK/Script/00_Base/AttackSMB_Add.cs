f8fa9c618cc70b631970bbb5dfad1b572ffd763d
ryi01 <kkjmo3@naver.com>
2026-07-13 11:56:16 +0900

    [Feat] 에셋 제거

--- Assets/KMK/Script/00_Base/AttackSMB_Add.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class AttackSMB_Add : StateMachineBehaviour
-{
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.GetComponent<EnemyAttackState>().IsAttack = true;
-    }
-}
--- Assets/KMK/Script/Player/Class/PlayerRuntimeState.cs
+++
@@ -1,9 +0,0 @@
-using System;
-[Serializable]
-public class PlayerRuntimeState
-{
-    public float CurrentHP;
-    public float SpeedMultiplier = 1f;
-    public float AttackBuffMultiplier = 1f;
-    public bool IsHit;
-}
